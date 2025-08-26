cc1862145d6c6fe900850e62c6b8a4142a389e16
Michael Dent <131711793+mikeld86@users.noreply.github.com>
2025-08-27 06:29:11 +0800

    a

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./**/*.{ts,tsx,html}", "!./node_modules/**", "!./dist/**"],
   theme: {
     extend: {
       borderRadius: {
@@ -91,5 +91,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
